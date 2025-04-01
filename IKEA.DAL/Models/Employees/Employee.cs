9da5920335ebc74cccc70142e53c5175b51a39a9
Abdulrahman Hamdy <abdul_rahman_hamdy_1824@fci.helwan.edu.eg>
2025-04-01 15:03:29 +0200

    Session-5_Video-1

---
+++ IKEA.DAL/Models/Employees/Employee.cs
@@ -0,0 +1,24 @@
+﻿using IKEA.DAL.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKEA.DAL.Models.Employees
+{
+	public class Employee:ModelBase
+	{
+        public string Name { get; set; }
+		public int? Age { get; set; }
+		public string? Address { get; set; }
+		public decimal Salary { get; set; }
+		public bool IsActive { get; set; }
+		public string Email { get; set; }
+		public string PhoneNumber { get; set; }
+		public DateOnly HiringDate { get; set; }
+		public Gender Gender { get; set; }
+		public EmpolyeeType EmpolyeeType { get; set; }
+
+	}
+}
--- IKEA.DAL/Persistance/Data/ApplicationDbContext.cs
+++ IKEA.DAL/Persistance/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using IKEA.DAL.Models.Departments;
+using IKEA.DAL.Models.Employees;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using System;
@@ -30,6 +31,7 @@ namespace IKEA.DAL.Persistance.Data
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
-        public DbSet<Department> Departments { get; set; }
-    }
+		public DbSet<Department> Departments { get; set; }
+		public DbSet<Employee> Employees { get; set; }
+	}
 }
